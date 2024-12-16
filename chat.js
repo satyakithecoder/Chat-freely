b1fc54004c7f7673a7212feaca16bad8fc9c4bb8
satyakithecoder <rajsatyaki2010@gmail.com>
2024-12-16 20:10:43 +0530

    files changed

--- chat.js
+++ chat.js
@@ -1,53 +1,114 @@
-import {getAnalytics} from "firebase/analytics";
-import {initializeApp} from "firebase/app";
-import { getFirestore, collection, addDoc} from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyB51Xx5xblhwbqez6forV-gXO7LwJmLwoA",
-    authDomain: "chat-freely-b9daa.firebaseapp.com",
-    projectId: "chat-freely-b9daa",
-    storageBucket: "chat-freely-b9daa.firebasestorage.app",
-    messagingSenderId: "533665648144",
-    appId: "1:533665648144:web:fd3081fbff1ae5e23fc495",
-    measurementId: "G-5T4E7ZDL7E"
+  apiKey: "AIzaSyB51Xx5xblhwbqez6forV-gXO7LwJmLwoA",
+  authDomain: "chat-freely-b9daa.firebaseapp.com",
+  projectId: "chat-freely-b9daa",
+  storageBucket: "chat-freely-b9daa.firebasestorage.app",
+  messagingSenderId: "533665648144",
+  appId: "1:533665648144:web:fd3081fbff1ae5e23fc495",
+  measurementId: "G-5T4E7ZDL7E",
 };
+
+function getQueryParam(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+if (getQueryParam("from") === "index") {
+  fetched();
+}
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app)
+const analytics = getAnalytics(app);
 const db = getFirestore();
 const auth = getAuth();
-
-
 const messagesRef = collection(db, "messages");
+const startTimestamp = new Date();
 
-const chatArea = document.querySelector("#chatArea")
-const messageInput = document.querySelector("#messageInput")
-const sendBtn = document.querySelector("#send")
-sendBtn.addEventListener("click", sendMessage)
-async function sendMessage() {
-    const messageText = messageInput.value;
-    const user = auth.currentUser;
-    if (user && messageText.trim() !== "") {
-        try {
-            await addDoc(messagesRef, {
-                userId: user.uid,
-                userName: user.displayName,
-                message: messageText,
-                timestamp: new Date()
-            });
-            addMessageToChat(messageText);
-        } catch (error) {
-            console.error("Error adding document: ", error);
-        }
+const messagesQuery = query(
+  messagesRef,
+  where("timestamp", ">", startTimestamp),
+  orderBy("timestamp"),
+);
+
+onSnapshot(messagesQuery, (snapshot) => {
+  snapshot.docChanges().forEach((change) => {
+    if (change.type === "added") {
+      const message = change.doc.data();
+      onChange(message);
     }
-    messageInput.value = "";
+  });
+});
+
+const chatArea = document.querySelector("#chatArea");
+const messageInput = document.querySelector("#messageInput");
+const sendBtn = document.querySelector("#send");
+sendBtn.addEventListener("click", sendMessage);
+
+async function sendMessage() {
+  const messageText = messageInput.value;
+  const user = auth.currentUser;
+  if (user && messageText.trim() !== "") {
+    const date = new Date();
+    try {
+      await addDoc(messagesRef, {
+        userId: user.uid,
+        userName: user.displayName,
+        message: messageText,
+        timestamp: date,
+      });
+      addMessageToChat(messageText, user.displayName, date);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  }
+  messageInput.value = "";
 }
-function addMessageToChat(message) {
-    const item = document.createElement("div");
-    item.classList.add("flex", "justify-end")
-    item.innerHTML = `
+
+function addMessageToChat(message, userName, timestamp) {
+  const item = document.createElement("div");
+  item.classList.add("flex", "justify-end");
+  item.innerHTML = `
       <div class="bg-blue-500 p-3 rounded-lg shadow text-white">
+                 <div class="text-sm text-gray-300">
+          <span>${userName}</span> • <span>${new Date(timestamp).toLocaleString()}</span>
+        </div>   
                     <p>${message}</p>
                 </div>
-    `
+    `;
+  chatArea.appendChild(item);
+}
+
+function onChange(message) {
+  const item = document.createElement("div");
+  const isCurrentUser = message.userId === auth.currentUser.uid;
+  if (!isCurrentUser) {
+    item.classList.add("flex");
+
+    item.innerHTML = `
+          <div class="bg-gray-800 text-gray-200 p-3 rounded-lg shadow">
+            <div class="text-sm text-gray-300">
+              <span>${message.userName}</span> • <span>${new Date(message.timestamp.toDate()).toLocaleString()}</span>
+            </div>
+            <p>${message.message}</p>
+          </div>
+        `;
     chatArea.appendChild(item);
+  }
+}
+
+function fetched() {
+  console.log("fetched");
 }
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-import {initializeApp} from "firebase/app";
-import {getAnalytics} from "firebase/analytics";
-import {getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
-import {doc, getFirestore, setDoc} from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB51Xx5xblhwbqez6forV-gXO7LwJmLwoA",
-    authDomain: "chat-freely-b9daa.firebaseapp.com",
-    projectId: "chat-freely-b9daa",
-    storageBucket: "chat-freely-b9daa.firebasestorage.app",
-    messagingSenderId: "533665648144",
-    appId: "1:533665648144:web:fd3081fbff1ae5e23fc495",
-    measurementId: "G-5T4E7ZDL7E"
+  apiKey: "AIzaSyB51Xx5xblhwbqez6forV-gXO7LwJmLwoA",
+  authDomain: "chat-freely-b9daa.firebaseapp.com",
+  projectId: "chat-freely-b9daa",
+  storageBucket: "chat-freely-b9daa.firebasestorage.app",
+  messagingSenderId: "533665648144",
+  appId: "1:533665648144:web:fd3081fbff1ae5e23fc495",
+  measurementId: "G-5T4E7ZDL7E",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -21,49 +26,51 @@ const providerGoogle = new GoogleAuthProvider();
 const providerGithub = new GithubAuthProvider();
 const google = document.getElementById("google-sign-in");
 google.addEventListener("click", () => {
-    signInWithPopup(auth, providerGoogle)
-        .then((result) => {
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const user = result.user;
-            setDoc(doc(db, "google", `${user.uid}`), {
-                name: user.displayName,
-                email: user.email,
-            })
-                .then(() => {
-                    console.log("User data saved successfully!");
-                })
-                .catch((error) => {
-                    console.error("Error saving user data:", error);
-                });
-            window.location.replace("chat.html");
-        }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.customData.email;
-        const credential = GoogleAuthProvider.credentialFromError(error);
+  signInWithPopup(auth, providerGoogle)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      setDoc(doc(db, "google", `${user.uid}`), {
+        name: user.displayName,
+        email: user.email,
+      })
+        .then(() => {
+          console.log("User data saved successfully!");
+        })
+        .catch((error) => {
+          console.error("Error saving user data:", error);
+        });
+      window.location.replace("chat.html?from=index");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
     });
 });
 document.getElementById("github-sign-in").addEventListener("click", () => {
-    signInWithPopup(auth, providerGithub)
-        .then((result) => {
-            const credential = GithubAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const user = result.user;
-            setDoc(doc(db, "github", `${user.uid}`), {
-                name: user.displayName,
-            })
-                .then(() => {
-                    console.log("User data saved successfully!");
-                })
-                .catch((error) => {
-                    console.error("Error saving user data:", error);
-                });
-            window.location.replace("chat.html");
-        }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.customData.email;
-        const credential = GithubAuthProvider.credentialFromError(error);
+  signInWithPopup(auth, providerGithub)
+    .then((result) => {
+      const credential = GithubAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      setDoc(doc(db, "github", `${user.uid}`), {
+        name: user.displayName,
+      })
+        .then(() => {
+          console.log("User data saved successfully!");
+        })
+        .catch((error) => {
+          console.error("Error saving user data:", error);
+        });
+      window.location.replace("chat.html?from=index");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GithubAuthProvider.credentialFromError(error);
     });
-})
+});
