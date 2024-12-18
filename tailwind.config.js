ae61bb9452c4f87ab6f13993e6235cbac3fcc0ab
satyakithecoder <rajsatyaki2010@gmail.com>
2024-12-18 20:55:59 +0530

    final commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./chat.html",
+    "./index.js",
+    "./chat.js",
+    // Add any other files where you use Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
